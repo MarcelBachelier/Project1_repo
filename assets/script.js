13b4cbc8ff1e9a3ee8d4b883bbf9de0f5abbdd12
jschuyl <schuylertjed@gmail.com>
2022-09-14 20:27:13 -0700

    got the bored api to display in html, working on parameters next

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,31 @@
+const baseURL = "http://www.boredapi.com/api/activity"
+const applySettings = "?"
+const adjustType = "&type="
+const adjustAccess = "&accessibility="
+const adjustParticipants = "&participants="
+const adjustPrice = "&price="
+const maxPrice = "&maxprice="
+const minPrice = "&minprice="
+
+
+function disperseData(data) {
+    currentActivity = document.getElementById('activity')
+    currentActivity.textContent = data.activity
+}
+
+function fetchActivity() {
+    fetch(baseURL)
+    .then(function (response) {
+        return response.json()
+})
+    .then(function (data) {
+        disperseData(data)   
+})
+}
+
+function iAmBored() {
+    shakeButton = document.getElementById('shakey-shake');
+    shakeButton.addEventListener('click', fetchActivity)   
+}
+iAmBored();
 
