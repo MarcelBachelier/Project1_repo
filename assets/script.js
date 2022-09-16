67b36cd2328ef87780689c5a80226dc077b38d25
emartinez1320 <Eliseomartinez840@yahoo.com>
2022-09-15 19:22:39 -0700

    testpush

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
+const baseURL = "http://www.boredapi.com/api/activity/"
+
+function disperseData(data){
+    currentActivity = document.getElementById('activity')
+    currentActivity.textContent = data.activity
+}
+
+function fetchActivity(){
+    fetch(baseURL)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data){
+        disperseData(data)
+    })
+}
+
+
+
+function iAmBored(){
+    shakeButton = document.getElementById("shakey-shake");
+    shakeButton.addEventListener("click", fetchActivity)
+}
+
+iAmBored();
 
