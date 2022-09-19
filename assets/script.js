1e2df9134c831206c758ba39e8bd232a327af73d
jschuyl <schuylertjed@gmail.com>
2022-09-19 16:09:28 -0700

    might be onto something witht hte gif thing

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ const adjustAccess = "&accessibility="
 const adjustParticipants = "&participants="
 const adjustPrice = "&price="
 const maxPrice = "&maxprice="
-const minPrice = "&minprice="
+const minPrice = "&minprice=0"
 
 function disperseData(data) {
     currentActivity = document.getElementById('activity')
@@ -22,15 +22,18 @@ function fetchActivity(boredURL) {
         disperseData(data) 
 })
 }
-
+// calls all the functions
 function iAmBored() {
     dropdownAccuracy = document.getElementById('type');
-    dropdownAccuracy.addEventListener('click', showPeople)
+    dropdownAccuracy.addEventListener('click', showPeople);
+
     shakeButton = document.getElementById('shakey-shake');
-    wholeABall = document.getElementById('outer')
+    wholeABall = document.getElementById('outer');
+
     shakeButton.addEventListener('click', applyChoices);
     shakeButton.addEventListener('click', shakeBall);
-    wholeABall.addEventListener('animationend', resetBall) 
+    wholeABall.addEventListener('animationend', resetBall);
+    shakeButton.addEventListener('click', resetGif) 
 }
 iAmBored();
 
@@ -38,16 +41,20 @@ function applyChoices() {
     let whatType = document.getElementById('type');
     let whatPrice = document.getElementById('price');
     let howManyPeople = document.getElementById('how-many-people');
+// forces one person unless stated otherwise in social and ONLY social
+    if (whatType.value != 'social') {
+        howManyPeople.value = 1
+    }
+        let priceURL = maxPrice + whatPrice.value;
+        let typeURL = adjustType + whatType.value;
+        let peopleURL = adjustParticipants + howManyPeople.value;
 
-    let priceURL = adjustPrice + whatPrice.value;
-    let typeURL = adjustType + whatType.value;
-    let peopleURL = adjustParticipants + howManyPeople.value;
-
-    let boredURL = baseURL + typeURL + priceURL + peopleURL;
-
+        let boredURL = baseURL + typeURL + minPrice + priceURL + peopleURL;
+    
     console.log(boredURL)
 
     fetchActivity(boredURL)
+    
 }
 
 function showPeople(){
@@ -59,18 +66,18 @@ function showPeople(){
         peopleChooser.classList.add('hide')
     }
 }
-
+// adds css classes to shake ball
 function shakeBall(){
     let ball = document.getElementById('outer')
     ball.classList.add('animate__animated', 'animate__wobble')
 }
-
+// removes classes so shakeBall() will be able to trigger again 
 function resetBall(){
     let resetBall = document.getElementById('outer');
     resetBall.classList.remove('animate__animated', 'animate__wobble')
 }
 
 function resetGif(){
-    let gif = document.getElementById('button-and-ball');
-    gif.reset()
+    let gif = document.getElementsByTagName("img");
+    gif.classList.add('animate__animated', 'animate__hinge')
 }
